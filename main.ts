789cdae9d00dfaf49a43b379e9558ff854cb1b4b
DX2000HACKER <117085999+DX2000HACKER@users.noreply.github.com>
2022-12-17 17:54:54 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+let A = 0
+input.onGesture(Gesture.Shake, function () {
+    A = randint(1, 4)
+    if (A == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (A == 2) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (A == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (A == 4) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
